b714bfe6cf661f139c8931f5bb53277ace84ed32
zhou.xuan <zhou.xuan@xgimi.com>
2024-02-26 20:52:59 +0800

    feat[菜单]:一级菜单

---
+++ web/src/view/my/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ myName }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'My',
+}
+</script>
+
+<script setup>
+import { ref } from 'vue'
+const myName = ref('name')
+</script>
